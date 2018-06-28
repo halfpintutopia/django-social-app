e88483039391e2a82fed3bbc308afda41daddaa4
Sirinya <30613818+halfpintutopia@users.noreply.github.com>
2018-06-28 17:33:19 +0200

    Post feed overview added and urls.py updated

--- social-app/project/api/urls.py
+++ social-app/project/api/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import logout
 # from django.views.generic import RedirectView
 
-from project.api.views.overview import OverviewView
+from project.api.views.overview import OverviewView, NewPostView, UserProfileView
 
 app_name = 'api'
 
@@ -14,5 +14,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', view=logout, name='logout'),
     # path('logout/', RedirectView.as_view(url='/')),
+    path('<int:user_id>/', UserProfileView.as_view(), name='user_profile'),
+    path('new-post/', NewPostView.as_view(), name='new_post'),
     path('', OverviewView.as_view(), name='overview'),
 ]
